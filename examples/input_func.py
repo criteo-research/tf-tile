4b5a43d837861c006102d153d122604e6074a884
h.maei <h.maei@criteo.com>
2019-06-21 17:58:11 +0000

    removed input_fn and model_fn and moved them into winequality_example

--- examples/input_func.py
+++
@@ -1,9 +0,0 @@
-def get_input_fn(dataset_fn, batch_size, tilings):
-    def input_fn():
-        dict_features, labels = dataset_fn().shuffle(2000).batch(
-            batch_size).repeat().make_one_shot_iterator().get_next()
-        features_dict = tilings.get_features_tiles(dict_features)
-
-        return features_dict, labels
-
-    return input_fn
