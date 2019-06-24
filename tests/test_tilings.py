0342155f5d241a757f1dfc775fb2493cb0490574
h.maei <h.maei@criteo.com>
2019-06-24 02:59:04 +0000

    added some test cases

--- tests/test_tilings.py
+++ tests/test_tilings.py
@@ -30,30 +30,5 @@ def test_tilings():
 	assert np.array_equal(bkts_1[0][0],[0, 4, 8]) and np.array_equal(bkts_2[0][0],[1, 5, 8])
 
 
-# def test_tf_tile():
-#     sess = tf.Session()
-
-#     num_buckets = 4
-#     num_tilings = 3
-#     feature_name = 'x'
-#     feature_range = {feature_name: [1, 5]}
-
-#     dataset = tf.data.Dataset.from_tensor_slices(tf.constant(
-#         [[1], [2], [3], [4], [5]]))  # -> Tuple[Callable[[], tf.data.Dataset], Callable[[], tf.data.Dataset]]:
-#     dataset = dataset.make_one_shot_iterator().get_next()
-#     data_dict = {feature_name: dataset}
-
-#     tile_strategy_boundaries = TileStrategy(feature_range).uniform(num_buckets)
-#     print("tile boundaries are:", tile_strategy_boundaries)
-#     tilings = Tilings(tile_strategy_boundaries, num_tilings)
-
-#     tile_column_list = TiledFeatureColumns(tilings).get_list()
-
-#     print(sess.run([data_dict[feature_name]]))
-#     data_in = tf.feature_column.input_layer(data_dict, tile_column_list)
-
-#     print(sess.run([data_in]))
-
-
 
 
